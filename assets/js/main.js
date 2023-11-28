31907a11be1d5d4051d127002b4369f1aa672d94
Renato Brito <dev.renatobrito@gmail.com>
2023-11-28 19:03:50 -0300

    :construction: Create some functionality with CRUD

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,50 @@
-const sumbitBtn = document.querySelector('#sumbitBtn');
+let productIdCounter = 0;
 
-sumbitBtn.addEventListener('click', event => {
-  event.preventDefault()
+function addProduct() {
+  const productName = document.querySelector("#productName").value;
+  const productValue = document.querySelector("#productValue").value;
+  const tableBody = document.querySelector("#tableBody");
 
-  const productName = document.querySelector('#productName').value;
-  const productValue = document.querySelector('#productValue').value;
+  const newRow = document.createElement("tr");
+  newRow.setAttribute("data-id", `product-${productIdCounter}`);
 
-  console.table([productName, productValue])
-})
+  newRow.innerHTML = `
+  <td>${productName}</td>
+  <td>${productValue}</td>
+  <td>
+    <button class="editButton" data-id="product-${productIdCounter}">Edit</button>
+    <button class="deleteButton" data-id="product-${productIdCounter}">Delete</button>
+  </td>
+  `;
+
+  tableBody.appendChild(newRow);
+  productIdCounter++;
+
+  document.querySelectorAll(".editButton").forEach((button) => {
+    button.addEventListener("click", editProduct);
+  });
+
+  document.querySelectorAll(".deleteButton").forEach((button) => {
+    button.addEventListener("click", deleteProduct);
+  });
+}
+
+function editProduct(event) {
+  const rowId = event.target.dataset.id;
+  const row = document.querySelector(`tr[data-id="${rowId}"]`);
+  const cells = row.querySelectorAll("td");
+
+  const productName = cells[0].innerText;
+  const productValue = cells[1].innerText;
+
+  document.querySelector("#productName").value = productName;
+  document.querySelector("#productValue").value = productValue;
+}
+
+function deleteProduct(event) {
+  const rowId = event.target.dataset.id;
+  const row = document.querySelector(`tr[data-id="${rowId}"]`);
+  row.remove();
+}
+
+document.querySelector("#submitButton").addEventListener("click", addProduct);
