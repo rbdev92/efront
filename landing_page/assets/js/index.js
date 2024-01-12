ea1630c09f067ed04f42a19b5d2b365de38ee86e
Renato Brito <dev.renatobrito@gmail.com>
2024-01-12 20:18:23 -0300

    :construction: Create stateRegister

--- landing_page/assets/js/index.js
+++ landing_page/assets/js/index.js
@@ -1,8 +1,30 @@
-document.querySelector(".btn__send").addEventListener("click", (e) => {
+const sendButton = document.querySelector(".btn__send");
+const closeButton = document.querySelector(".btn__close");
+const errorMessage = document.querySelector(".error__message");
+const successMessage = document.querySelector(".success__message");
+const modalSend = document.querySelector(".modal__send");
+
+const success = "#04d361";
+const error = "#ff3e3e";
+
+sendButton.addEventListener("click", (e) => {
   e.preventDefault();
-  document.querySelector(".modal__send").showModal();
+
+  const stateRegister = "error";
+
+  if (stateRegister === "success") {
+    errorMessage.remove();
+    closeButton.style.backgroundColor = success;
+  }
+  
+  if (stateRegister === "error") {
+    successMessage.remove();
+    closeButton.style.backgroundColor = error;
+  }
+
+  modalSend.show();
 });
 
-document.querySelector(".btn__close").addEventListener("click", () => {
-  document.querySelector(".modal__send").close();
+closeButton.addEventListener("click", () => {
+  modalSend.close();
 });
